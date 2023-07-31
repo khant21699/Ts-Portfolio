e4fab441f238e3c2c22d759fb29f4c850dcd5722
khant21699 <khant21699@hotmail.com>
2023-07-31 15:41:48 +0630

    footer fix

--- src/Pages/Footer.tsx
+++ src/Pages/Footer.tsx
@@ -6,7 +6,7 @@ export default function Footer({}: Props) {
   return (
     <div className=" w-full h-[100px] border-secondary border-t-[1px] font-firaCode text-secondary flex justify-center items-center">
       <h2>
-        © Copyright 2023. Mady by <span className=" text-primary">Khant</span>
+        © Copyright 2023. Made by <span className=" text-primary">Khant</span>
       </h2>
     </div>
   );
